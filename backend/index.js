7cb7cba323175c6e8667da7acbb313c361227bd0
keertiPawar8 <keertipawar8@gmail.com>
2023-05-08 08:26:29 +0530

    code of day-5

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,9 @@ const cors = require("cors")
 const app = express();
 app.use(express.json())
 app.use(cors())
-
+app.get("/",()=>{
+    res.send("HOME PAGE")
+})
 app.use("/",userRouter)
 app.use("/",quesRouter)
 
