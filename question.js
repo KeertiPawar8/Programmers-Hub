3a588ccb69765d4d1c298842922c7d43f6a0fb80
Vipin4147 <vipin4147@gmail.com>
2023-05-10 00:05:08 +0530

    frontend deployed

--- question.js
+++ question.js
@@ -7,34 +7,36 @@ let send_data = async () => {
   if (topic == "" || question == "") {
     alert("please fill all the fields");
     return;
-  } else {
-    let obj = {
-      topic,
-      question,
-    };
-    try {
-      let data = await fetch(
-        "https://rose-betta-cape.cyclic.app/createquestion/",
-        {
-          method: "POST",
-          headers: {
-            "Content-type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-          body: JSON.stringify(obj),
-        }
-      );
-      data = await data.json();
-      console.log(data);
-      alert(data.msg);
-    } catch (error) {
-      console.log(error);
-    }
   }
+  // else {
+  let obj = {
+    topic,
+    question,
+  };
+  try {
+    let data = await fetch(
+      "https://rose-betta-cape.cyclic.app/createquestion",
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(obj),
+      }
+    );
+    data = await data.json();
+    console.log(data);
+    alert(data.msg);
+  } catch (error) {
+    console.log(error);
+  }
+  // }
 };
 
 let btn1 = document.getElementById("btn1");
-btn1.onclick = () => {
+btn1.onclick = (e) => {
+  e.preventDefault();
   send_data();
 };
 
